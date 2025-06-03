8b3094ff4d30817b5f2fb6a7eda25516a4b59c6c
kimsumin <tnals1564@naver.com>
2025-06-03 15:28:06 +0900

    project setting

---
+++ src/main/java/com/smsoft/java21virtualthreadplayground/Java21VirtualThreadPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.smsoft.java21virtualthreadplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java21VirtualThreadPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java21VirtualThreadPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smsoft/java21virtualthreadplayground/Java21VirtualThreadPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smsoft.java21virtualthreadplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java21VirtualThreadPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
